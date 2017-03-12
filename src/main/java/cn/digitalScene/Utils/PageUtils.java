0e2d49f3503a4792441612e0903b0e394e083462
yang <2506597416@qq.com>
2017-03-13 00:53:34 +0800

    当前页码的判断写成工具类

--- src/main/java/cn/digitalScene/Utils/PageUtils.java
+++ src/main/java/cn/digitalScene/Utils/PageUtils.java
@@ -4,4 +4,19 @@ package cn.digitalScene.Utils;
  * Created by 25065 on 2017/3/12.
  */
 public class PageUtils {
+
+    //返回当前的页码
+    public static Integer returnPageCurrent(Integer pageCurrent,Integer parameterCountNow,Integer parameterCountBefore,Integer pageNumbers){
+        if (pageCurrent==null||pageCurrent.equals("")){
+            pageCurrent=0;
+        }else if(parameterCountNow!=parameterCountBefore){
+            pageCurrent=0;
+//            parameterCountBefore=parameterCountNow;
+        }else if (pageCurrent>pageNumbers){
+            pageCurrent=0;
+        }else {
+            pageCurrent=pageCurrent-1;
+        }
+        return pageCurrent;
+    }
 }
